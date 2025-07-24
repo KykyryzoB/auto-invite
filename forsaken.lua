c53e4c13e8b451249b7aced97c37509570da23bb
KykyryzoB <148477723+KykyryzoB@users.noreply.github.com>
2025-07-24 20:04:56 +0500

    Update forsaken.lua

--- forsaken.lua
+++ forsaken.lua
@@ -2,12 +2,12 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 repeat wait()
-until game.Player.LocalPlayer.Character
+until game.Players.LocalPlayer.Character
 wait(1)
 game:GetService("TextChatService").TextChannels:WaitForChild("RBXGeneral"):SendAsync("кто хочет на ру сервер фарсакен пиши мне в лс Inet_4 или нажми F9 и в конце будет ссылка")
 wait(1)
 
-local text = "\n https://discord.gg/WznZtMrbh8"
+local text = "\n НАШ \n СЕРВЕР \n ТУТ \n discord.gg/WznZtMrbh8 -- НАШ ЛУЧШИЙ СЕРВЕР"
  
 local local_player = game:GetService("Players").LocalPlayer
 local animate = local_player.Character.Animate
